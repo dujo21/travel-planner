8aa5a4874b752fb2278d9dd905127c054629e8e6
Dusan <dusanloncar14@gmail.com>
2026-08-14 18:14:20 +0200

    dodao ISharingService interfejs i ShareToken model za remoting

---
+++ backend/TravelPlanner/TravelPlanner.Contracts/ISharingService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Services.Remoting;
+
+namespace TravelPlanner.Contracts
+{
+    public interface ISharingService : IService
+    {
+        Task<ShareToken> CreateShareAsync(Guid tripId, Guid ownerUserId, string accessType, int? expiryDays);
+        Task<ShareToken?> ValidateTokenAsync(Guid token);
+        Task<List<ShareToken>> GetSharesForTripAsync(Guid tripId);
+        Task<bool> RevokeShareAsync(Guid token);
+    }
+}
---
+++ backend/TravelPlanner/TravelPlanner.Contracts/ShareToken.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace TravelPlanner.Contracts
+{
+    [DataContract]
+    public class ShareToken
+    {
+        [DataMember]
+        public Guid Token { get; set; }
+
+        [DataMember]
+        public Guid TripId { get; set; }
+
+        [DataMember]
+        public Guid OwnerUserId { get; set; }
+
+        [DataMember]
+        public string AccessType { get; set; } = "VIEW"; // "VIEW" ili "EDIT"
+
+        [DataMember]
+        public DateTime CreatedAt { get; set; }
+
+        [DataMember]
+        public DateTime? ExpiresAt { get; set; }
+
+        [DataMember]
+        public bool IsRevoked { get; set; }
+    }
+}
